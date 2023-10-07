80607cc3dc54f846918914224c07066624435c70
vuthehung <hungvt280102@gmail.com>
2023-10-07 10:32:15 +0700

    connect mongodb

--- src/app.js
+++ src/app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const { default: helmet } = require('helmet')
 const morgan = require('morgan')
 const app = express()
-
 // init middlewares
 app.use(morgan('dev'))
 app.use(helmet())
 // intit db
-
+require('./dbs/init.mongodb')
 // init routes
 app.get('/', (req, res, next) => {
     return res.status(200).json({
